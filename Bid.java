cf7a09b796ec936e608f1f23c3de70853a36bc65
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-04-21 05:05:59 +0700

    Add method name
    - Add all method in bid
    - Fix some indentation in auction

--- Bid.java
+++ Bid.java
@@ -1,13 +1,53 @@
+import java.util.Date;
 
-public class Bid {
-
-	public Bid(User user, int money) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public int getMoney() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+/**
+ * Represent the bid object in auction program.
+ * Contain the bidder, money, and bid date.
+ * 
+ * Created by Kla & Tong
+ * 14 April 2020
+ */
+public class Bid
+{
+    /** Bidder of the bid */
+    User bidder;
+    
+    /** Amount of money that bid **/
+    int money;
+    
+    /** Date that bid **/
+    Date dateBid;
+    
+    /** 
+     * Constructor of bid for a new bid
+     * @param user
+     * @param money
+     */
+    public Bid(User user, int money)
+    {
+        this.bidder = user;
+        this.money = money;
+        this.dateBid = IOUtils.getCurrentDateTime();
+    }
+    
+    public User getBidder()
+    {
+        return this.bidder;
+    }
+    
+    public Date getDateTime()
+    {
+        return this.dateBid;
+    }
+    
+    public int getMoney()
+    {
+        return this.money;
+    }
+    
+    public int compareTo(Bid bid)
+    {
+        return this.money - bid.getMoney();
+    }
 
 }
