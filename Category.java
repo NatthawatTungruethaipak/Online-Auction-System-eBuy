806df007e99ef7c58845a84145f9915b743766f1
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-18 20:28:43 +0700

    Implement the function

--- Category.java
+++ Category.java
@@ -1,16 +1,46 @@
+import java.util.ArrayList;
 
 public class Category {
-
-	public String getCategory()
-	{
+	private static ArrayList<Category> categoryList = new ArrayList<Category>();
 	
-		return null;
-	}
-
-	public static Category findCategory(String category)
+	private String category;
+	
+	public static Category findCategory(String categoryString)
 	{
-		
-		return null;
+		Category retCategory = null;
+		for(Category category: categoryList)
+		{
+			if(category.getCategoryString().equals(categoryString))
+			{
+				retCategory = category;
+				break;
+			}
+		}
+		if(retCategory == null)
+			retCategory = Category.findCategory("Other");
+		return retCategory;
 	}
-
+	
+	public static ArrayList<String> getAllCategoryString()
+	{
+		ArrayList<String> retList = new ArrayList<String>();
+		for(Category category: categoryList)
+		{
+			retList.add(category.getCategoryString());
+		}
+		return retList;
+	}
+	
+	public String getCategoryString()
+	{
+		return category;
+	}
+	
+	public int equals(String categoryString)
+	{
+		if(category == categoryString)
+			return 0;
+		else
+			return 1;
+	}
 }
