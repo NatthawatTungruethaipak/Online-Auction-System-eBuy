d99bb7c0ef15561b44d9c5beffac13fb09874434
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-18 21:20:48 +0700

    Add comment

--- Category.java
+++ Category.java
@@ -1,16 +1,55 @@
 import java.util.ArrayList;
 
-public class Category {
+/**
+ * Category for item in auction.
+ * 
+ * Created by Kla & Tong
+ * 18 May 2020
+ */
+public class Category
+{
+	/** All category **/
 	private static ArrayList<Category> categoryList = new ArrayList<Category>();
 	
-	private String category;
+	/** Category string **/
+	private String categoryStr;
 	
-	public static Category findCategory(String categoryString)
+	/**
+	 * Constructor of Category. Set the string and add the Category to list
+	 * @param categoryStr String of category
+	 */
+	public Category(String categoryStr)
+	{
+		this.categoryStr = categoryStr; 
+		categoryList.add(this);
+	}
+	
+	/**
+	 * Initialise the Category when starting the program
+	 */
+	public static void initial()
+	{
+		String[] strArray = {"Electronics", "Collecibles & Art", "Fashion",
+							"Home & Garden", "Sporting Goods", "Health & Beauty",
+							"Toys & Hobbies", "Books", "Movies", "Music",
+							"Business & Industrial", "Vehicle", "Other"};
+		for (String str: strArray)
+		{
+			new Category(str);
+		}
+	}
+	
+	/**
+	 * Find the category instance from all category
+	 * @param categoryFind String of category that want to get
+	 * @return Category instance that finding from string. If not found, Category of "Other".
+	 */
+	public static Category findCategory(String categoryFind)
 	{
 		Category retCategory = null;
 		for(Category category: categoryList)
 		{
-			if(category.getCategoryString().equals(categoryString))
+			if(category.equals(categoryFind))
 			{
 				retCategory = category;
 				break;
@@ -21,26 +60,40 @@ public class Category {
 		return retCategory;
 	}
 	
-	public static ArrayList<String> getAllCategoryString()
+	/**
+	 * Get the category string list
+	 * @return Array list of category in string format.
+	 */
+	public static ArrayList<String> getAllCategoryStr()
 	{
 		ArrayList<String> retList = new ArrayList<String>();
 		for(Category category: categoryList)
 		{
-			retList.add(category.getCategoryString());
+			retList.add(category.getCategoryStr());
 		}
 		return retList;
 	}
 	
-	public String getCategoryString()
+	/**
+	 * Get category string of current instance.
+	 * @return Category string inside.
+	 */
+	public String getCategoryStr()
 	{
-		return category;
+		return this.categoryStr;
 	}
 	
-	public int equals(String categoryString)
+	/**
+	 * Override the equals method to compare from string
+	 * @param categoryStr Category string that want to compare
+	 * @return True if string inside category and string that want
+	 * 			to compare is the same. Otherwise, False.
+	 */
+	public boolean equals(String categoryStr)
 	{
-		if(category == categoryString)
-			return 0;
+		if(this.categoryStr == categoryStr)
+			return true;
 		else
-			return 1;
+			return false;
 	}
 }
