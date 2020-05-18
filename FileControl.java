09a959337ea7bdb119fad12cc9903cc7f2d73d56
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-18 22:15:18 +0700

    Add data variable

--- FileControl.java
+++ FileControl.java
@@ -1,4 +1,15 @@
+import java.io.*;
 
-public class FileControl {
-
+/**
+ * Basic File Controller. Open file, read file, write file, etc.
+ * 
+ * Created by Kla & Tong
+ * 18 May 2020
+ */
+public class FileControl
+{
+	private File file;
+	private String fileName;
+	private BufferedWriter writer;
+	private BufferedWriter reader;
 }
