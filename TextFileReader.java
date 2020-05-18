c1f5c52ba8ed861661eaeff9c9696395653c0d0d
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-19 03:26:56 +0700

    Implement TextFileReader
    - Implement method

--- TextFileReader.java
+++ TextFileReader.java
@@ -8,21 +8,63 @@ public class TextFileReader
 	
 	public TextFileReader(String fileName)
 	{
-		
+		this.fileName = fileName; 
 	}
 	
 	public boolean openFile()
 	{
-		
+		try
+		{
+			if(reader != null)
+			{
+				reader.close();
+				reader = null;
+			}
+			reader = new BufferedReader(new FileReader(fileName));
+		}
+		catch(Exception e)
+		{
+			System.out.println("System: Cannot open the read file");
+			reader = null;
+			return false;
+		}
+		return true;
 	}
 	
 	public boolean closeFile()
 	{
-		
+		try
+		{
+			if(reader != null)
+			{
+				reader.close();
+				reader = null;
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("System: Cannot close read file, some error occur.");
+			reader = null;
+			return false;
+		}
+		return true;
 	}
 	
 	public String readLine()
 	{
-		
+		if(reader == null)
+			return null;
+		String line;
+		try
+		{
+			line = reader.readLine();
+		}
+		catch (IOException e)
+		{
+			System.out.println("System: Error from reading file.");
+			closeFile();
+			return null;
+		}
+		return line;
 	}
 }
