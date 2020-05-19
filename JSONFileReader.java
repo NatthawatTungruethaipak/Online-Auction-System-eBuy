7bb229b674947b9ae7bb9fc39f82ff8f7c073d79
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-19 16:11:14 +0700

    Change from TextFile to JSONFile
    - Changing from TextFileReader and TextFileWriter to JSONFileReader and JSONFileWriter
    - put JSON-SIMPLE jar file in the same path

---
+++ JSONFileReader.java
@@ -0,0 +1,103 @@
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Read file control. Can open, close, and read json from file.
+ * This class uses the external class, JSON-SIMPLE.
+ * Reference: https://code.google.com/archive/p/json-simple/
+ * 
+ * Created by Kla & Tong
+ * 19 May 2020
+ */
+public class JSONFileReader
+{
+	/** File name to open **/
+	String fileName = null;
+	
+	/** File reader **/
+	FileReader fileReader;
+	
+	/**
+	 * Constructor of TextFileReader. Set the file name
+	 * @param fileName
+	 */
+	public JSONFileReader(String fileName)
+	{
+		this.fileName = fileName; 
+	}
+	
+	/**
+	 * Open JSON file.
+	 * @return Return true if can open. Otherwise, false.
+	 */
+	private boolean open()
+	{
+		boolean bCheck = false;
+		try
+		{
+			if(fileReader != null)
+			{
+				fileReader.close();
+				fileReader = null;
+			}
+			fileReader = new FileReader(fileName);
+			bCheck = true;
+		}
+		catch(Exception e)
+		{
+			System.out.println("System: Error, Cannot open the read file");
+			fileReader = null;
+		}
+		return bCheck;
+	}
+	
+	/**
+	 * Close file
+	 * @return Return true if can close. If there are error occur, false
+	 */
+	private boolean close()
+	{
+		boolean bCheck = false;
+		try
+		{
+			if(fileReader != null)
+			{
+				fileReader.close();
+				fileReader = null;
+			}
+			bCheck = true;
+		}
+		catch(Exception e)
+		{
+			System.out.println("System: Error, cannot close the read file.");
+			fileReader = null;
+		}
+		return bCheck;
+	}
+	
+	/**
+	 * Read line from text file
+	 * @return Line read from text file
+	 */
+	public JSONArray readJSON()
+	{
+		open();
+		if(fileReader == null)
+			return null;
+		JSONParser jsonParser = new JSONParser();
+		Object obj = null;
+		try
+		{
+			obj = jsonParser.parse(fileReader);
+		}
+		catch (Exception e)
+		{
+			System.out.println("System: Error, cannot reading file.");
+			close();
+			return null;
+		}
+		close();
+		return (JSONArray) obj;
+	}
+}
