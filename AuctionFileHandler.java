e68ee89a18f510d493afb4a47d9428e6d9ec3196
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-19 20:50:52 +0700

    Implement read user file
    - Implement read user
    - Implement parse user

--- AuctionFileHandler.java
+++ AuctionFileHandler.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
 /**
  * Manage the Auction file
  * 
@@ -37,28 +34,77 @@ public class AuctionFileHandler
 	{
 		return auctionFileHandler;
 	}
+
+	public User parseUser(String parse[])
+	{
+		String username = null;
+		String password = null;
+		String name = null;
+		String surname = null;
+		Date birth = null;
+		String address = null;
+		String email = null;
+		int balance = 0;
+		for(int i = 0; i < 8; i++)
+		{
+			 String fields[] = parse[i].split(" ", 2);
+			 if(parse[0].equals("USERNAME") && IOUtils.validateUsername(parse[1]))
+				username = parse[1];
+			 else if(parse[0].equals("PASSWORD") && IOUtils.validatePassword(parse[1]))
+				password = parse[1];
+			 else if(parse[0].equals("NAME") && IOUtils.isNullStr(parse[1]) != true)
+				name = parse[1];
+			 else if(parse[0].equals("SURNAME") && IOUtils.isNullStr(parse[1]) != true)
+				 surname = parse[1];
+			 else if(parse[0].equals("BIRTH") && IOUtils.validateDate(parse[1]))
+				 birth = IOUtils.createTimeInstance(parse[1]);
+			 else if(parse[0].equals("ADDRESS"))
+				 address = parse[1];
+			 else if(parse[0].equals("EMAIL") && IOUtils.validateEmail(parse[1]))
+				 email = parse[1];
+			 else if(parse[0].equals("BALANCE") && IOUtils.validateInteger(parse[1]))
+				 balance = Integer.parseInt(parse[1]);
+			 else
+				 return null;
+		}
+		User user = new User(username, password, name, surname, birth, address, email);
+		user.addMoney(balance);
+		return user;
+	}
 	
 	/**
 	 * Read user file and add to system.
 	 */
 	public ArrayList<User> readUsers()
 	{
-		JSONFileReader userReader = new JSONFileReader(userFileName);
-		JSONArray userJSON = userReader.readJSON();
-		return ;		
+		String parameter[];
+		int count = 0;
+		ArrayList<User> userList = new ArrayList<User>();
+		
+		/** Open file. If cannot open, return null; **/
+		TextFileReader reader = new TextFileReader(userFileName);
+		if(reader.open() == false)
+			return null;
+		
+		/** Read number of data **/
+		String line;
+		line = reader.readLine();
+		count = Integer.parseInt(line);
+		
+		/** Loop get user and add to list**/
+		for(int i = 0; i < count; i++)
+		{
+			parameter = new String[8];
+			for(int j = 0; i < 8;i++)
+		    	parameter[i] = reader.readLine();
+			User user = parseUser(parameter);
+			if(user != null) /* If data is correct, add to user list */  
+				userList.add(user);
+		}
+		return userList;
+		
 	}
-	
-	private User parseUserObject(JSONObject userObj)
-	{
-		String username = (String) userObj.get("username");
-		String password = (String) userObj.get("password");
-		String name = (String) userObj.get("name");
-		String surname = (String) userObj.get(surname);
-		Date birth = (Date) userObj.get();
-		String address;
-		String email;
-		int balance;
-	}
+
 	
 	/**
 	 * Read auction file and add to system.
@@ -90,4 +136,6 @@ public class AuctionFileHandler
 	{
 		
 	}
+	
+	private User
 }
