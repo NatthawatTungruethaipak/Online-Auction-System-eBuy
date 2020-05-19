9ee6a38bdb5f69a5b4043988b4da87b1f7fb519b
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-19 10:52:54 +0700

    Add comment to AuctionFileHandler

--- AuctionFileHandler.java
+++ AuctionFileHandler.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Manage the Auction file
  * 
@@ -6,41 +8,63 @@
  */
 public class AuctionFileHandler
 {
-	final String userFileName = "auctionData.txt";
+	/** User file name **/
+	final String userFileName = "userData.txt";
 	
+	/** Auction file name **/
 	final String auctionFileName = "auctionData.txt";
 	
+	/** Instance of AuctionFileHandler. Implement singleton **/
 	private AuctionFileHandler auctionFileHandler = new AuctionFileHandler();
 	
+	/**
+	 * Constructor of AuctionFileHandler.
+	 * Prevent another class can create the instance. Implement for singleton
+	 */
 	private AuctionFileHandler()
 	{
 	}
 	
+	/**
+	 * Get instance of AuctionFileHandler. Implement singleton
+	 * @return
+	 */
 	public static AuctionFileHandler getSingletonInstance()
 	{
 		
 	}
 	
-	public void initial()
-	{
-		
-	}
-	
+	/**
+	 * Read user file and add to system.
+	 */
 	private void readUsers()
 	{
 		
 	}
 	
+	/**
+	 * Read auction file and add to system.
+	 */
 	private void readAuctions()
 	{
 		
 	}
 	
-	public boolean writeUsers(ArrayList<User> auctions)
+	/**
+	 * Write users data to user text file.
+	 * @param users Users that going to write
+	 * @return True if don't have any problem.
+	 */
+	public boolean writeUsers(ArrayList<User> users)
 	{
 		
 	}
 	
+	/**
+	 * Write auction data to auction text file
+	 * @param auctions Auctions that going to write
+	 * @return True if don't have any problem.
+	 */
 	public boolean writeAuctions(ArrayList<Auction> auctions)
 	{
 		
