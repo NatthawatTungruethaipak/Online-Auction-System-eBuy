0b3bc285b900635ef7556f855738501885c104f3
Nathpahop <nathaphop.s@mail.kmutt.ac.th>
2020-05-19 10:39:24 +0700

    add method and header comment

--- AuctionFileHandler.java
+++ AuctionFileHandler.java
@@ -1,4 +1,48 @@
-
-public class AuctionFileHandler {
-
+/**
+ * Manage the Auction file
+ * 
+ * Created by Kla & Tong
+ * 19 May 2020
+ */
+public class AuctionFileHandler
+{
+	final String userFileName = "auctionData.txt";
+	
+	final String auctionFileName = "auctionData.txt";
+	
+	private AuctionFileHandler auctionFileHandler = new AuctionFileHandler();
+	
+	private AuctionFileHandler()
+	{
+	}
+	
+	public static AuctionFileHandler getSingletonInstance()
+	{
+		
+	}
+	
+	public void initial()
+	{
+		
+	}
+	
+	private void readUsers()
+	{
+		
+	}
+	
+	private void readAuctions()
+	{
+		
+	}
+	
+	public boolean writeUsers(ArrayList<User> auctions)
+	{
+		
+	}
+	
+	public boolean writeAuctions(ArrayList<Auction> auctions)
+	{
+		
+	}
 }
